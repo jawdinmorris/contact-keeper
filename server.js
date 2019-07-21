43cf667950972393da82accfc8a683003d9cfe2b
jawdinmorris <jordanandrewmorris1@gmail.com>
2019-07-21 15:00:19 +1000

    chap40

--- server.js
+++ server.js
@@ -4,5 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.json({ msg: "Welcome to contact keeper api" }));
-
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+
+//Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));

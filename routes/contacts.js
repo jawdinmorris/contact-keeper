1ee98e1fc4ea1c146f1b234de25c1316a0e9e05c
jawdinmorris <jordanandrewmorris1@gmail.com>
2019-07-23 10:33:25 +1000

    Create contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -8,9 +8,40 @@ const Contact = require("../models/Contact");
 // @route   POST api/contacts
 // @desc    Create a users contact
 // @access  Private
-router.post("/", (req, res) => {
-  res.send("Create a new contact");
-});
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("name", "Name is reuired")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // @route   GET api/contacts
 // @desc    Get all users contact
